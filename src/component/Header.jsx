d92600f354c123725afaac90d50253710f046e26
modtherhub <moibhub@gmail.com>
2025-06-04 11:09:27 +0400

    first commit

---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='absolute w-full z-50 transition-all 
+    duration-300'>
+       hea
+    </div>
+  )
+}
+
+export default Header
