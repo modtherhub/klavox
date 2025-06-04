8b612379498e2fc4067c6f428b1bb451be705a2e
modtherhub <moibhub@gmail.com>
2025-06-04 11:56:58 +0400

    Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='absolute w-full z-50 transition-all 
+    <header className='absolute w-full z-50 transition-all 
     duration-300'>
-       hea
-    </div>
+      <div className='container mx-auto px-4 sm:px-6 
+       lg:px-8 flex items-center justify-between'>
+        
+        {/* logo/Name */}
+        <div className='flex items-center'>
+          <div className='h-10 w-10 rounded-xl bg-gradient-to-r
+          from-gray-500 to-gray-100 flex '>
+            
+          </div>
+
+        </div>
+      </div>
+    </header>
   )
 }
 
-export default Header
+export default Header;
